c8b3bd24bbd2596537fb88753720ae65ec438289
kristian0808 <kristianlleshaj@hotmail.com>
2025-05-04 20:58:32 +0200

    feat: Implement authentication flow with login page and context management

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path"
+import { fileURLToPath } from 'url' // Import fileURLToPath
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -7,4 +9,11 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      // Use import.meta.url to get the current file's URL, convert it to a path,
+      // get the directory name, and then resolve the 'src' directory relative to that.
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
+    },
+  },
 })
