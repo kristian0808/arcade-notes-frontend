83913b131fdc4cb085115774f780f195796dc26e
kristian0808 <kristianlleshaj@hotmail.com>
2025-06-15 23:02:19 +0200

    feat: Update search button visibility to show when in search mode instead of active tabs

--- src/components/MemberList/MemberList.tsx
+++ src/components/MemberList/MemberList.tsx
@@ -300,7 +300,7 @@ const MemberList: React.FC<MemberListProps> = ({
               aria-label="Search members"
               autoFocus
             />
-            {showActiveTabsOnly && (
+            {isSearchMode && (
               <button
                 onClick={handleCloseSearch}
                 className="absolute right-3 top-1/2 transform -translate-y-1/2 p-0.5 text-gray-400 dark:text-gray-500 hover:text-gray-600 dark:hover:text-gray-300 rounded transition-colors"
